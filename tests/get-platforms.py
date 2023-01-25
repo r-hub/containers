7d268485fa69ef4f112bb3dae32ecedc9e2bd893
Gábor Csárdi <csardi.gabor@gmail.com>
2023-01-25 09:04:44 +0100

    GHA: allow building a subset of R builds only

--- tests/get-platforms.py
+++ tests/get-platforms.py
@@ -2,6 +2,17 @@
 
 import yaml
 import json
+import argparse
+
+parser = argparse.ArgumentParser(description="Print R-builds platforms as JSON.")
+parser.add_argument(
+    'platforms',
+    type = str,
+    nargs = '?',
+    default = 'all',
+    help = 'Comma-separated list of platforms. Default is "all" to use all platforms.'
+)
+args = parser.parse_args()
 
 with open("docker-compose.yml", "r") as stream:
     try:
@@ -9,6 +20,9 @@ with open("docker-compose.yml", "r") as stream:
         serv = conf.get("services")
         keys = serv.keys()
         skeys = [ str(key) for key in keys ]
+        if args.platforms != 'all':
+            platforms = args.platforms.split(",")
+            skeys = [ p for p in platforms if p in skeys ]
         print(json.dumps(skeys))
 
     except yaml.YAMLError as exc:
