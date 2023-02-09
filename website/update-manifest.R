cde4fbf65b971bef1e55dab3067b11de9eff2daa
Gábor Csárdi <csardi.gabor@gmail.com>
2023-02-09 16:10:39 +0100

    Better manifest update (incomplete)

--- website/update-manifest.R
+++ website/update-manifest.R
@@ -1,9 +1,3 @@
 
 pkgload::load_all()
-if (! tolower(Sys.getenv("UPDATE_MANIFEST", "yes")) %in%
-    c("no", "false", "off", "0")) {
-  pull_containers()
-}
-
-# we always need to create a manifest file
 update_manifest()
