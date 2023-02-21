c69bbfd577a97113cd6fcedd3bc94d982acba678
Gábor Csárdi <csardi.gabor@gmail.com>
2023-02-21 12:14:42 +0100

    Include OS and R version in main website table

--- website/R/html.R
+++ website/R/html.R
@@ -1,20 +1,4 @@
 
-format_manifest <- function() {
-  cat(r"(
-```
-this is is verbatim
-```
-  )")
-}
-
-format_tag <- function(tag) {
-  ""
-}
-
-format_container <- function(id) {
-  ""
-}
-
 format_id <- function(id) {
   id <- sub("^ghcr.io/r-hub/containers/[-.a-zA-Z0-9]+@sha256:", "", id)
   substr(id, 1, 7)
@@ -24,4 +8,26 @@ format_tag <- function(tag) {
   tag <- sub("ghcr.io/r-hub/containers/", "", tag, fixed = TRUE)
   tag <- sub(":latest", "", tag, fixed = TRUE)
   tag
+}
+
+get_cont_data <- function(id) {
+  conts <- load_new_manifest()
+  tags <- format_tag(purrr::map_chr(conts$containers, "tag"))
+  if (!id %in% tags) stop("Unknown container: ", id)
+  conts$containers[[which(tags == id)]]$builds[[1]]
+}
+
+os_name <- function(id) {
+  cont <- get_cont_data(id)
+  os_release <- strsplit(cont$`/etc/os-release`, "\n", fixed = TRUE)[[1]]
+  pn <- grep("^PRETTY_NAME", os_release, value = TRUE)[1]
+  pn <- sub("^PRETTY_NAME=", "", pn)
+  pn <- unquote(pn)
+  pn
+}
+
+r_ver <- function(id) {
+  cont <- get_cont_data(id)
+  sess <- strsplit(cont$`sessionInfo()`, "\n", fixed = TRUE)[[1]]
+  sess[1]
 }
--- website/R/utils.R
+++ website/R/utils.R
@@ -6,3 +6,11 @@ first_char <- function(x) {
 last_char <- function(x) {
   substr(x, nchar(x), nchar(x))
 }
+
+unquote <- function(x) {
+  ifelse(
+    first_char(x) == last_char(x) & first_char(x) %in% c("'", '"'),
+    substr(x, 2L, nchar(x) - 1L),
+    x
+  )
+}
