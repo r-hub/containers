30fc73d5b75f4700d5f19b04f46c5d1febd2028d
Gábor Csárdi <csardi.gabor@gmail.com>
2023-02-24 12:19:32 +0100

    Fix check script on clang-asan
    Now LinkingTo dependencies are compiled from source.

--- containers/clang-asan/install-deps.R
+++ containers/clang-asan/install-deps.R
@@ -1,21 +1,23 @@
 
 install_deps <- function(path) {
-
+  options(pak.no_extra_messages = TRUE)
   deps <- pak::pkg_deps(
     paste0("deps::", normalizePath(path)), dependencies = FALSE
   )$deps[[1]]
 
   lnkto <- deps$ref[deps$type == "LinkingTo"]
-  if (length(knkto == 0)) return()
-
-  # Install LinkingTo deps from source
-  refs <- paste0(lnkto, "?source")
-  pak::pkg_install(refs)
+  if (length(lnkto) != 0) {
+    message("LinkingTo dependencies: ", paste(lnkto, collapse = ", "), ".")
+    repos <- getOption("repos")
+    options(repos = repos["CRAN"])
+    pak::pkg_install(lnkto)
+    options(repos = repos)
+  }
 
   # Install others, potentitally from binaries
   pak::pkg_install(paste0("deps::", path), dependencies = TRUE)
 }
 
-if (is.null(sys.calls()) {
+if (is.null(sys.calls())) {
   install_deps(commandArgs(TRUE)[1])
 }
