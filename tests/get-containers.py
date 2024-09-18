d54b2db6f35dd12b2e04ad73a9b70ebcc76b4ec2
Gábor Csárdi <csardi.gabor@gmail.com>
2024-09-18 13:57:04 +0200

    Add RHEL containers

--- tests/get-containers.py
+++ tests/get-containers.py
@@ -19,11 +19,21 @@ if args.containers != 'all':
     containers = args.containers.split(",")
     conts = [ p for p in containers if p in conts ]
 
-# some hardcoded platforms
-platforms = { 's390x': 'linux/s390x' }
+# some hardcoded platforms and secrets
+platforms = {
+    's390x': 'linux/s390x',
+    'rhel8': 'linux/amd64,linux/arm64',
+    'rhel9': 'linux/amd64,linux/arm64'
+}
+secrets = {
+    'rhel7': 'REDHAT_ORG_RHEL7,REDHAT_ACTIVATION_KEY_RHEL7',
+    'rhel8': 'REDHAT_ORG_RHEL8,REDHAT_ACTIVATION_KEY_RHEL8',
+    'rhel9': 'REDHAT_ORG_RHEL9,REDHAT_ACTIVATION_KEY_RHEL9',
+}
 conf = [ {
     'container': c,
-    'platform': platforms[c] if c in platforms else 'linux/amd64'
+    'platform': platforms[c] if c in platforms else 'linux/amd64',
+    'secrets': secrets[c] if c in secrets else ''
 } for c in conts ]
 
 print(json.dumps(conf))
